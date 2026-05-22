86271aaae5957025434943a301cb29d9afcf8c53
isdeyeter <edestechbilisim@gmail.com>
2026-05-22 15:33:51 +0300

    fix: use astro 4 with cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server', // Bu çok önemli!
+  output: 'server',
+  adapter: cloudflare(),
   integrations: [tailwind()],
 });
